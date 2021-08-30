19b940d21899278272e19fbad84548ab10492cff
ismarps <ismarprobst@gmail.com>
2021-08-30 19:05:23 -0300

    first commit

---
+++ src/main/java/springapp/microblogdynaccurate/MicroblogDynaccurateApplication.java
@@ -0,0 +1,13 @@
+package springapp.microblogdynaccurate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroblogDynaccurateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroblogDynaccurateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springapp/microblogdynaccurate/MicroblogDynaccurateApplicationTests.java
@@ -0,0 +1,13 @@
+package springapp.microblogdynaccurate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroblogDynaccurateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
