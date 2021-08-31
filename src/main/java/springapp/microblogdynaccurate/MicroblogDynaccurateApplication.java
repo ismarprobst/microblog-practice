e972b16a8e5aac7473aee0c0d276ab056daa2b37
ismarps <ismarprobst@gmail.com>
2021-08-30 23:20:17 -0300

    Criação da classe do Swagger

--- src/main/java/springapp/microblogdynaccurate/MicroblogDynaccurateApplication.java
+++ src/main/java/springapp/microblogdynaccurate/MicroblogDynaccurateApplication.java
@@ -2,9 +2,20 @@ package springapp.microblogdynaccurate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class MicroblogDynaccurateApplication {
+	
+	@GetMapping
+	public ModelAndView swaggerUI() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroblogDynaccurateApplication.class, args);
